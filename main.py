f7d7c757902d95301e9ed8ea2143e3a57be74734
jeandmalves <jeandmalves@gmail.com>
2023-05-27 20:09:01 -0300

    troca do da variavel executable_path por um input

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from utils.general_information import GeneralInformation
 from utils.process import Process
 
-executable_path = "Coloque o caminho do executável relativo ou absoluto aqui"
-
+executable_path = input("Digite o caminho absoluto até o executável: ")
 systemInformation = GeneralInformation()
 process = Process()
 
 print("Primeiro algumas informações sobre o sistema:")
+print()
 systemInformation.print_system_information()
 systemInformation.print_virtual_memory()
 
 print("------------------------------------------------------------------------------")
 print("Agora vamos executar o programa passado e coletar algumas informações:")
+print()
 process.exec(path = executable_path)
